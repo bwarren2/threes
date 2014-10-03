bc99fbc8c172691c1395d2855dc6a92380ab5a9c
Ben Warren <bwarren2@gmail.com>
2014-10-02 21:19:21 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='threes',
-    version='0.1.11',
+    version='0.1.12',
     description='Implementing the tile merge game',
     long_description=readme + '\n\n' + history,
     author='Ben Warren',
